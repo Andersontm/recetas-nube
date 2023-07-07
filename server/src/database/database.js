731c4c49f885f4ba77df7fda09a27ff291ed6bcb
Andersontm <anderson.tm7@gmail.com>
2023-07-07 14:20:47 -0500

    Version 2.0.3 Update DB

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,11 +2,11 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("railway", "postgres", "eA0G4AJ6vC9wtmSKsLq7", {
-  host: "containers-us-west-155.railway.app",
+export const sequelize = new Sequelize("railway", "postgres", "TlIYOECCAZuoOHMppfQg", {
+  host: "containers-us-west-192.railway.app",
   logging: false,
   dialect: "postgres",
-  postgres:7442,
+  port: 7106,
 });
 
 
